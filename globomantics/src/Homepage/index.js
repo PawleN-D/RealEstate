91e5c03642f135c59ed99cde06cbf5af6178be58
PawleN-D <sirpawle@gmail.com>
2023-09-25 12:48:00 +0200

    add hooks and fetch data

--- globomantics/src/Homepage/index.js
+++ globomantics/src/Homepage/index.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './homepage.css';
 import Header from './header';
 
+
 function App() {
+
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const resp = await fetch('/houses.json');
+      const houses = await resp.json();
+      setAllHouses(houses)
+    };
+    fetchHouses();
+  }, [])
+
+  let featuredHouse = {};
+
+  if (allHouses.length) {
+    const randomIndex = Math.floor(Math.random() * allHouses.length)
+    featuredHouse = allHouses[randomIndex];
+  }
   return (
     <div className="container">
-      <Header />
+      <Header subtitle='Providing houses all over the world!' />
     </div>
   );
 }
