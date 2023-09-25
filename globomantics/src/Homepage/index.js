7235c9753f9ff22f4bcec2583395953fdf5281e8
PawleN-D <sirpawle@gmail.com>
2023-09-25 12:26:31 +0200

    add header to the app

---
+++ globomantics/src/Homepage/index.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './homepage.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
